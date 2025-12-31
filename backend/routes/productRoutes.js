d2238e6d0ceb727d0216c4721559a6244ba2d942
Grishmacoder <gcpatel888@gmail.com>
2025-12-31 16:28:22 -0500

    protecting APIS routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,15 +8,19 @@ const {
   updatePrice,
 } = require("../controllers/productController");
 const upload = require("../middleware/upload");
+const requireAuth = require("../middleware/requireAuth");
+
+//auth for all routes
+router.use(requireAuth);
 
 router.get("/", getCakes);
 
 router.get("/:id", getCakeById);
 
-router.post("/",upload.single('image'), createCake);
+router.post("/", upload.single("image"), createCake);
 
 router.delete("/:id", deleteCake);
 
-router.patch("/:id",upload.single('image'), updatePrice);
+router.patch("/:id", upload.single("image"), updatePrice);
 
 module.exports = router;
